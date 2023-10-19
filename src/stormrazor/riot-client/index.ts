9eccd08e9a3bc5564f67fb19ff7b1d9c99d2ef75
Ray <dev@hiray.me>
2023-10-19 01:36:02 +0100

    Add hashes to files for riot client to differentiate between files

--- src/stormrazor/riot-client/index.ts
+++ src/stormrazor/riot-client/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import tracer from 'tracer';
 import path from 'path';
 import { flattenObject } from '../../util/object';
+import { addmd5HashtoFile } from '../../util/crypto';
 
 const logger = tracer.colorConsole();
 
@@ -27,44 +28,55 @@ const processManifest = async (url: string, dir: string) => {
 
             logger.warn(`Attempting to download ${dlurl}`);
 
+            const fileName = dlurl.substring(dlurl.lastIndexOf('/') + 1);
+
             try {
                 if (dlurl.includes('tft')) {
                     // Tft
                     logger.warn('Attempting to download tft riot-client');
                     await download(dlurl, `${dir}/riot-client/tft/`);
+                    addmd5HashtoFile(`${dir}/riot-client/tft/${fileName}`);
                 } else if (dlurl.includes('arcane')) {
                     // Arcane
                     logger.warn('Attempting to download arcane riot-client');
                     await download(dlurl, `${dir}/riot-client/arcane/`);
+                    addmd5HashtoFile(`${dir}/riot-client/arcane/${fileName}`);
                 } else if (dlurl.includes('wildrift')) {
                     // Wildrift
                     logger.warn('Attempting to download wildrift riot-client');
                     await download(dlurl, `${dir}/riot-client/wr/`);
+                    addmd5HashtoFile(`${dir}/riot-client/wr/${fileName}`);
                 } else if (dlurl.includes('valorant')) {
                     // Valorant
                     logger.warn('Attempting to download val riot-client');
                     await download(dlurl, `${dir}/riot-client/val/`);
+                    addmd5HashtoFile(`${dir}/riot-client/val/${fileName}`);
                 } else if (dlurl.includes('bacon')) {
                     // Legends of runterra
                     logger.warn('Attempting to download lor riot-client');
                     await download(dlurl, `${dir}/riot-client/lor/`);
+                    addmd5HashtoFile(`${dir}/riot-client/lor/${fileName}`);
                 } else if (dlurl.startsWith('https://lol.secure.dyn.riotcdn.net/channels/public/rccontent')) {
                     // League of legends
                     logger.warn('Attempting to download lol riot-client');
                     await download(dlurl, `${dir}/riot-client/lol/`);
+                    addmd5HashtoFile(`${dir}/riot-client/lol/${fileName}`);
                 } else if (
                     dlurl.startsWith('https://riot-client.secure.dyn.riotcdn.net/channels/public/rccontent/theme')
                 ) {
                     // Riot client
                     logger.warn('Attempting to download riot-client riot-client');
                     await download(dlurl, `${dir}/riot-client/riot-client/`);
+                    addmd5HashtoFile(`${dir}/riot-client/riot-client/${fileName}`);
                 } else {
                     // Unknown
                     logger.warn('Attempting to download unknown riot-client');
                     await download(dlurl, `${dir}/riot-client/unknown/`);
+                    addmd5HashtoFile(`${dir}/riot-client/unknown/${fileName}`);
                 }
             } catch (e) {
                 logger.error(`Failed to download: ${dlurl}`);
+                logger.error(e);
             }
         }
     }
--- src/util/crypto.ts
+++ src/util/crypto.ts
@@ -1,4 +1,6 @@
 import crypto from 'node:crypto';
+import fs from 'fs-extra';
+import path from 'path';
 
 /**
  * Encodes the text given into a md5 string using the built-in node implementation.
@@ -8,4 +10,28 @@ import crypto from 'node:crypto';
  */
 export const md5HashEncode = (data: string): string => {
     return crypto.createHash('md5').update(data.trim()).digest('hex');
+};
+
+/**
+ * Adds a truncated(to 7 characters) md5 hash(using the contents of the file) to the filename.
+ * @param filepath {string} The file to add a hash to.
+ */
+export const addmd5HashtoFile = (filePath: string): void => {
+    const contents = fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+    const hash = crypto.createHash('md5').update(contents.trim()).digest('hex');
+
+    const truncatedHash = hash.substring(0, 7);
+
+    const existingFileName = filePath.substring(filePath.lastIndexOf(path.sep) + 1);
+
+    const fileNameArr = existingFileName.split('.');
+
+    fileNameArr.splice(1, 0, truncatedHash);
+
+    const newFileName = fileNameArr.join('.');
+
+    const existingFilePath = filePath.substring(0, filePath.lastIndexOf(path.sep) - 1);
+
+    fs.renameSync(filePath, path.join(existingFilePath, newFileName));
 };
